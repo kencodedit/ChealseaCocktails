aeb1ceaa6ee789f0189f271df2ac26055ed3eb20
kenneth <kenteze97@yahoo.com>
2022-07-22 23:55:48 +0100

    First commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Chealsea started making cocktails in 1997. She has grown her passion for
+        cocktails into a successful cocktail club called Chealsea's cocktails.
+        These clubs occur around the country with branches in London, manchester
+        and Birmingham. You can be sure to enjoy a tasty glass of cocktail when
+        you join the club.You can search some of the cocktails which are made at
+        chealsea's and see the recipe and try to replicate or infuse your own
+        flavour to these wonderfully crafted drinks. Enjoy drinking !
+      </p>
+    </section>
+  );
+};
+
+export default About;
